90484374e7f59b9bb7ea497eb4a67a843017e0ea
Thomas Schnapka <thomas.schnapka@outlook.com>
2023-01-05 12:53:57 +0100

    more realtistic star sizes in plot

--- src/plot.py
+++ src/plot.py
@@ -2,6 +2,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 from copy import deepcopy
 
+# https://www.astronomy.ohio-state.edu/ryden.1/ast162_2/notes9.html
+BETELGEUSE_RAD = 0.125*(1/3600)*np.pi/180 # angular size in RAD
+BETELGEUSE_MAG  = 0.58
+
+FIGSIZE_INCH = 12 # figure is square
+FIG_DPI      = 150
+
 def plot_star_selection(sc, ra_center, dec_center, fov, deg_ticks=False, 
                         tan_proj=False):
     """
@@ -28,14 +35,20 @@ def plot_star_selection(sc, ra_center, dec_center, fov, deg_ticks=False,
     dec = sc.dec[choice]
     mag = sc.mag[choice]
 
-    size = 100*(1.5**-mag)
+    #size = 100*(1.5**-mag)
+
+    # size relative to FOV
+    #relative_size = (BETELGEUSE_RAD/fov)*BETELGEUSE_MAG/mag#/(2.512**-BETELGEUSE_MAG)*(2.512**-mag)
+    relative_size = (BETELGEUSE_RAD/fov)/(2.512**-BETELGEUSE_MAG)*(2.512**-mag)
+    points_in_fov = FIGSIZE_INCH*FIG_DPI
+    size = 1e3*4*points_in_fov*relative_size
     
     if tan_proj==True:
         ra = np.tan(ra - ra_center) + ra_center
         dec = np.tan(dec - dec_center) + dec_center
 
     plt.style.use('dark_background')
-    fig, ax = plt.subplots(figsize=(12,12))
+    fig, ax = plt.subplots(figsize=(FIGSIZE_INCH, FIGSIZE_INCH), dpi=FIG_DPI)
     ax.set_xlim(ra_center-fov/2, ra_center+fov/2)
     ax.set_ylim(dec_center-fov/2, dec_center+fov/2)
     ax.scatter(ra, dec, s=size, c="w")
@@ -50,6 +63,8 @@ def plot_star_selection(sc, ra_center, dec_center, fov, deg_ticks=False,
         ax.set_xticks(xticks, [f"{ang:.3f}°" for ang in xticks*180/np.pi],
                       rotation=-30)
         ax.set_yticks(yticks, [f"{ang:.3f}°" for ang in yticks*180/np.pi])
+        ax.set_ylabel("dec [deg]")
+        ax.set_xlabel("ra [deg]")
     
     # add names
     names = sc.name[choice]
@@ -99,8 +114,8 @@ def draw_grid_cells(ax, grid):
         kwargs = {"linewidth": 1, "color":"r", "alpha":0.6/(i+1), "label":"cells"}
 
         # calculate grid
-        grid_ra = _grid.ra_start   + np.arange(0, _grid.n_ra)*_grid.ra_width
-        grid_dec = _grid.dec_start + np.arange(0, _grid.n_dec)*_grid.dec_width
+        grid_ra = _grid.ra_start   + np.arange(0, _grid.n_ra+1)*_grid.ra_width
+        grid_dec = _grid.dec_start + np.arange(0, _grid.n_dec+1)*_grid.dec_width
         
         # plot grid
         for ra in grid_ra: ax.axvline(ra, np.pi, -np.pi, **kwargs)
